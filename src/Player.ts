23eb8e6ec156845f7b273d6f21146b1e83fa33cf
Miguel Afonso <migueljrafonso@gmail.com>
2024-10-15 14:48:32 +0200

    add card mapping - value to point

--- src/Player.ts
+++ src/Player.ts
@@ -25,10 +25,7 @@ export class Player {
   public hole_cards: Card[] = [];
   public version = '';
   public id = 0;
-  public cardMappings = {
-    '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-    '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14
-  };
+  public CARD_MAPPING = { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14 };
 
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
     const { community_cards, players } = gameState;
