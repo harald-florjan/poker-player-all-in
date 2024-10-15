57b98894e2e3749c25a7bf9c88594348e440a795
Jessica Gomez <jessica.gomez@avrios.com>
2024-10-15 14:18:15 +0200

    init commit

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,32 @@
+export interface GameState {
+  players: Player[];
+  tournament_id: string;
+  game_id: string;
+  round: number;
+  bet_index: number;
+  small_blind: number;
+  orbits: number;
+  dealer: number;
+  community_cards: Card[];
+  current_buy_in: number;
+  pot: number;
+}
+
+export interface Card {
+  rank: string;
+  suit: string;
+}
 export class Player {
+  public name = '';
+  public stack = 0;
+  public status = '';
+  public bet = 0;
+  public hole_cards = [];
+  public version = '';
+  public id = 0;
+
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+    betCallback(2);
   }
 
   public showdown(gameState: any): void {
