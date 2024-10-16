f3b6ffe537dd32f91f78bd6113ea7ea67320af66
Harald Florjan <harald.florjan@avrios.com>
2024-10-16 09:19:26 +0200

    add interface

--- src/Player.ts
+++ src/Player.ts
@@ -15,8 +15,31 @@ export interface GameState {
 }
 
 export interface Card {
-  rank: string;
-  suit: string;
+  rank: Rank;
+  suit: Suit;
+}
+
+export enum Rank {
+  TWO = '2',
+  THREE = '3',
+  FOUR = '4',
+  FIVE = '5',
+  SIX = '6',
+  SEVEN = '7',
+  EIGHT = '8',
+  NINE = '9',
+  TEN = '10',
+  JACK = 'J',
+  QUEEN = 'Q',
+  KING = 'K',
+  ACE = 'A'
+}
+
+export enum Suit {
+  HEARTS = 'hearts',
+  SPADES = 'spades',
+  DIAMONDS = 'diamonds',
+  CLUBS = 'clubs'
 }
 
 export class Player {
