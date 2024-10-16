1c35c144351ab67bf92252207af892c446c8fab2
Miguel Afonso <migueljrafonso@gmail.com>
2024-10-16 11:52:14 +0200

    outplayers to 6

--- src/Player.ts
+++ src/Player.ts
@@ -24,7 +24,7 @@ export class Player {
     let outPlayers = gameState.players.filter(player => player.status === 'out');
     console.log('outPlayers', outPlayers);
 
-    if (outPlayers.length < 5) {
+    if (outPlayers.length < 6) {
       betCallback(0);
       return;
     }
